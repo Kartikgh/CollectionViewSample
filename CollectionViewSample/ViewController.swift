9a82600fb3327ce479910c0e430aba8afa26cf52
Karthik <karthikkurdekar@gmail.com>
2025-02-14 23:32:14 +0530

    Initial Commit

---
+++ CollectionViewSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewSample
+//
+//  Created by Karthik.Kurdekar on 14/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
